4246aa6f33fd7b0c19735558d31f66a24312a0bd
Gabriel Zambrano <gab.zambrano@gmail.com>
2020-06-26 16:38:29 +0200

    salsa fusion v2

--- resources/views/courses/online.blade.php
+++ resources/views/courses/online.blade.php
@@ -52,11 +52,11 @@
                         </button>
                     </form>
                     @else
-                    @endif
                     <a href="{{ route('dashboard') }}"
                         class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
                         Dashboard
                     </a>
+                    @endif
                     @else
                     <a href="{{ route('login') }}"
                         class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
@@ -158,11 +158,11 @@
                         </button>
                     </form>
                     @else
-                    @endif
                     <a href="{{ route('dashboard') }}"
                         class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
                         Dashboard
                     </a>
+                    @endif
                     @else
                     <a href="{{ route('login') }}"
                         class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
@@ -198,11 +198,11 @@
         </button>
     </form>
     @else
-    @endif
     <a href="{{ route('dashboard') }}"
         class="w-48 flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
         Dashboard
     </a>
+    @endif
 
     @else
     <a href="{{ route('register') }}"
