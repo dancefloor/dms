697edcacdf6399184c316a774c816f4af7ead814
Gabriel Zambrano <gab.zambrano@gmail.com>
2020-08-13 11:57:15 +0200

    improve catalogue design

--- resources/views/components/course-daily-schedule.blade.php
+++ resources/views/components/course-daily-schedule.blade.php
@@ -1,8 +1,8 @@
-<div class="text-gray-600 text-sm">
+<div class="text-gray-600 text-sm mb-1">
     <span class="inline-flex items-center">
         @include('icons.time', ['style' => 'w-4 mr-2'])
         @foreach ($dailySchedule as $item)
-        {{ $item['day'] }} {{ $item['start_time'] }} {{ $item['end_time'] }}
+        {{ $item['day'] }} {{ $item['start_time'] }} {{ $item['end_time'] }} <br>
         @endforeach
     </span>
 </div>
