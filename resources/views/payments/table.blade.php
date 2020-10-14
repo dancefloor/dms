8b44a2328c21a7ba729d2f443dd431782df4cf80
Gabriel Zambrano <gab.zambrano@gmail.com>
2020-10-14 11:24:16 +0200

    payments order id table commented

--- resources/views/payments/table.blade.php
+++ resources/views/payments/table.blade.php
@@ -17,7 +17,7 @@
                 <td class="px-3">{{ $payment->method }}</td>
                 <td class="px-3">{{ $payment->amount }}</td>
                 <td class="px-3">{{ $payment->code }}</td>
-                <td class="px-3">{{ $payment->order->id }}</td>
+                <td class="px-3">{{ $payment->order->id ?? '' }}</td>
                 <td class="px-3 flex justify-end">
                     <a href="{{ route('payments.show', $payment) }}" class="hover:underline mx-2">Show</a>
                     {{-- <a href="{{ route('payments.edit', $payment) }}" class='btn btn-default btn-xs'>Edit</a> --}}
