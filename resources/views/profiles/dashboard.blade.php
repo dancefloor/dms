25781ad9ff661152c9cf33d14d2757f580ddc9fb
Gabriel Zambrano <gab.zambrano@gmail.com>
2020-10-16 14:18:49 +0200

    fb group button and text improvements v4

--- resources/views/profiles/dashboard.blade.php
+++ resources/views/profiles/dashboard.blade.php
@@ -47,7 +47,7 @@
                 <div class="capitalize text-right">
                     @if ($item->pivot->status === 'registered')
                     <a href="{{$item->online_link}}"
-                        class="w-full my-1 flex items-center justify-center px-2 py-1 border border-transparent text-xs rounded-full text-white bg-blue-700 hover:bg-blue-600 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition duration-150 ease-in-out md:py-1 md:text-lg md:px-2"
+                        class="my-1 items-center justify-center px-3 py-1 border border-transparent text-xs rounded-full text-white bg-blue-700 hover:bg-blue-600 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue transition duration-150 ease-in-out md:py-1 md:text-sm md:px-3"
                         target="_blank">FB Group</a>
                     @endif
                     @if ($item->pivot->status === 'pre-registered')
